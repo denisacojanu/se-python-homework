7050134c6c629050cd39d1d85ebe80c7e9284485
Denisa Cojanu <denisan.cojanu@gmail.com>
2021-10-09 13:14:05 +0300

    Rezolvari exercitii 21-22

--- session2/ex21.py
+++ session2/ex21.py
@@ -10,3 +10,12 @@
         Veti primi: 'cmi', 'center', 'for', 'machines'
         Veti printa: ['cm', 'cente', 'fo', 'machine']
 """
+# Rezolvare
+x = input()
+l1 = []
+while x != 'exit' and len(x) >= 1:
+    y = len(x)
+    z = x[:y-1]
+    l1.append(z)
+    x = input()
+print(l1)
--- session2/ex22.py
+++ session2/ex22.py
@@ -6,3 +6,32 @@
         Veti primi: 'center'
         Veti printa: 'CeNtEr'
 """
+#Rezolvare metodata 1 cu liste
+x = input()
+#declaram o lista goala
+l1 = []
+#caracterele din x le punem in lista l1
+l1[:0] = x
+l2 = []
+for i in range(len(l1)):
+    if i % 2 == 0:
+        l2.append(l1[i].upper())
+    else:
+        l2.append(l1[i].lower())
+
+#in x punem elementele din lista l2 care contine caracterele de pe pozitiile pare cu litere mari
+x = ''.join(map(str, l2))
+print(x)
+
+#Rezolvare metoda 2 fara liste
+x = input()
+y = ""
+for i in range(len(x)):
+    if i % 2 == 0:
+        y = y + x[i].upper()
+    else:
+        y = y + x[i].lower()
+print(y)
+
+
+
