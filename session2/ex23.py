ac6da2d59cca3b4a4d67b25414d68eed60ba721f
Denisa Cojanu <denisan.cojanu@gmail.com>
2021-10-10 12:21:12 +0300

    Rezolvare ex 23

--- session2/ex23.py
+++ session2/ex23.py
@@ -14,3 +14,28 @@
         Veti primi: 'cojoc'
         Veti printa: True
 """
+
+# # Rezolvare 1
+x = input()
+# In y stocam valoarea introdusa de la tastatura inversata
+y = x[::-1]
+if x == y:
+    print(True)
+else:
+    print(False)
+
+# Rezolvare 2
+z = input()
+flag = False
+for i in range(0, int(len(z)/2)):
+    if z[i] == z[len(z)-i-1]:
+        flag = True
+print(flag)
+
+# Rezolvare 3
+a = input()
+rev = ''.join(reversed(a))
+if a == rev:
+    print(True)
+else:
+    print(False)
