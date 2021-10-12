3788ba29c559e33b1dd8772ae19086ef3b328784
Denisa Cojanu <denisa.cojanu@jpard.com>
2021-10-12 19:13:56 +0300

    Rezolvari exercitii 28-29

--- session2/ex28.py
+++ session2/ex28.py
@@ -6,3 +6,10 @@
         Veti primi: 5
         Veti printa: 15
 """
+# Rezolvare
+x = int(input("Introduceti un numar: "))
+suma = 0
+
+for i in range(1, x+1): # 0 este considerat element neutru in adunare si de aceea am pornit de la 1
+    suma = suma + i
+print("Suma numerelor este: ", suma)
--- session2/ex29.py
+++ session2/ex29.py
@@ -9,3 +9,35 @@
         2 (pentru vocale)
         4 (pentru consoane)
 """
+
+# Rezolvare metoda 1
+x = input("Introduceti un cuvant: ")
+vocale = ['a', 'e', 'i', 'o', 'u']
+
+nr_vocale = 0
+nr_consoane = 0
+
+x = x.replace(' ', '')  # Am adaugat replace deoarece daca introducem 2 cuvinte, spatiul era vazut ca o consoana
+for i in x:
+    if i in vocale:
+        nr_vocale += 1
+    elif i not in vocale and i != ' ':
+        nr_consoane += 1
+print("Numarul de vocale este: ", nr_vocale)
+print("Numarul de consoane este: ", nr_consoane)
+
+# Rezolvare metoda 2
+x = input("Introduceti un cuvant: ")
+vocale = ['a', 'e', 'i', 'o', 'u']
+
+nr_vocale = 0
+nr_consoane = 0
+
+x = x.replace(' ', '')  # Am adaugat replace deoarece daca introducem 2 cuvinte, spatiul era vazut ca o consoana
+for i in x:
+    if i in vocale:
+        nr_vocale += 1
+    else:
+        nr_consoane += 1
+print("Numarul de vocale este: ", nr_vocale)
+print("Numarul de consoane este: ", nr_consoane)
