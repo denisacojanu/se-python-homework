942c4051d99c59a06a063fe8bea3e6244a558a99
Denisa Cojanu <denisan.cojanu@gmail.com>
2021-10-16 18:29:33 +0300

    Rezolvare exercitiu 30

--- session2/ex30.py
+++ session2/ex30.py
@@ -10,3 +10,25 @@
         Veti primi: '(()]'
         Veti printa: False
 """
+
+# Rezolvare
+x = input()
+
+l1 = []  # am creeat o lista goala
+paranteze = {")": "(", "]": "[", "}": "{"}  # stocam fiecare paranteza inchisa ca avand valoarea parantezei deschise
+
+count = 0
+for c in x:
+    if c in paranteze:
+        if l1 and l1[-1] == paranteze[c]:
+            l1.pop()
+        else:
+            break
+    else:
+        l1.append(c)
+    count = count + 1
+
+if not l1 and count != 0:
+    print(True)
+else:
+    print(False)
